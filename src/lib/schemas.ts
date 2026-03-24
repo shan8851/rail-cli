545b96a4dfd1c5e60ea5ed6ea3c6837db38bf30c
shan8851 <asamshan456@gmail.com>
2026-03-24 13:24:14 +0000

    chore: fix schemas

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -12,7 +12,7 @@ export const StationSearchResponseSchema = z.array(StationSearchResultSchema);
 export const ServiceLocationSchema = z
   .object({
     assocIsCancelled: z.boolean().optional(),
-    crs: z.string().optional(),
+    crs: z.string().optional().nullable(),
     futureChangeTo: z.string().optional().nullable(),
     locationName: z.string(),
     via: z.string().optional().nullable(),
@@ -21,44 +21,44 @@ export const ServiceLocationSchema = z
 
 export const CallingPointSchema = z
   .object({
-    at: z.string().optional(),
-    crs: z.string().optional(),
-    et: z.string().optional(),
+    at: z.string().optional().nullable(),
+    crs: z.string().optional().nullable(),
+    et: z.string().optional().nullable(),
     isCancelled: z.boolean().optional(),
     locationName: z.string(),
-    st: z.string().optional(),
+    st: z.string().optional().nullable(),
   })
   .passthrough();
 
 export const CallingPointListSchema = z
   .object({
-    callingPoint: z.array(CallingPointSchema).optional().default([]),
+    callingPoint: z.array(CallingPointSchema).optional().nullable().default([]),
   })
   .passthrough();
 
 export const BoardServiceSchema = z
   .object({
-    ata: z.string().optional(),
-    atd: z.string().optional(),
-    cancelReason: z.string().optional(),
-    currentDestinations: z.array(ServiceLocationSchema).optional().default([]),
-    currentOrigins: z.array(ServiceLocationSchema).optional().default([]),
-    delayReason: z.string().optional(),
-    destination: z.array(ServiceLocationSchema).optional().default([]),
-    eta: z.string().optional(),
-    etd: z.string().optional(),
+    ata: z.string().optional().nullable(),
+    atd: z.string().optional().nullable(),
+    cancelReason: z.string().optional().nullable(),
+    currentDestinations: z.array(ServiceLocationSchema).optional().nullable().default([]),
+    currentOrigins: z.array(ServiceLocationSchema).optional().nullable().default([]),
+    delayReason: z.string().optional().nullable(),
+    destination: z.array(ServiceLocationSchema).optional().nullable().default([]),
+    eta: z.string().optional().nullable(),
+    etd: z.string().optional().nullable(),
     filterLocationCancelled: z.boolean().optional(),
     isCancelled: z.boolean().optional(),
-    operator: z.string().optional(),
-    operatorCode: z.string().optional(),
-    origin: z.array(ServiceLocationSchema).optional().default([]),
-    platform: z.string().optional(),
-    previousCallingPoints: z.array(CallingPointListSchema).optional().default([]),
-    rsid: z.string().optional(),
-    serviceID: z.string().optional(),
-    sta: z.string().optional(),
-    std: z.string().optional(),
-    subsequentCallingPoints: z.array(CallingPointListSchema).optional().default([]),
+    operator: z.string().optional().nullable(),
+    operatorCode: z.string().optional().nullable(),
+    origin: z.array(ServiceLocationSchema).optional().nullable().default([]),
+    platform: z.string().optional().nullable(),
+    previousCallingPoints: z.array(CallingPointListSchema).optional().nullable().default([]),
+    rsid: z.string().optional().nullable(),
+    serviceID: z.string().optional().nullable(),
+    sta: z.string().optional().nullable(),
+    std: z.string().optional().nullable(),
+    subsequentCallingPoints: z.array(CallingPointListSchema).optional().nullable().default([]),
   })
   .passthrough();
 
@@ -66,14 +66,14 @@ export const StationBoardResponseSchema = z
   .object({
     areServicesAvailable: z.boolean().optional(),
     crs: z.string(),
-    filterLocationName: z.string().optional(),
-    filterType: z.enum(['from', 'to']).optional(),
-    filtercrs: z.string().optional(),
+    filterLocationName: z.string().optional().nullable(),
+    filterType: z.union([z.enum(['from', 'to']), z.number()]).optional().nullable(),
+    filtercrs: z.string().optional().nullable(),
     generatedAt: z.string(),
     locationName: z.string(),
-    nrccMessages: z.array(z.unknown()).optional().default([]),
+    nrccMessages: z.array(z.unknown()).optional().nullable().default([]),
     platformAvailable: z.boolean().optional(),
-    trainServices: z.array(BoardServiceSchema).optional().default([]),
+    trainServices: z.array(BoardServiceSchema).optional().nullable().default([]),
   })
   .passthrough();
 
